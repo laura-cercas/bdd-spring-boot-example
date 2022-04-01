0e1d6305d96f590ad7e36356f01e6b07006cd0bb
Laura Cercas <laura.cercas@autentia.com>
2022-04-01 13:06:12 +0200

    Initial Commit

---
+++ src/main/java/com/tutorial/bddspringbootexample/BddSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.bddspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddSpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BddSpringBootExampleApplication.class, args);
+    }
+
+}
